ac6e19da884029d295df7f730f51d6cd8117e92e
iurimach <119754002+iurimach@users.noreply.github.com>
2023-05-28 16:45:44 +0400

    dialog 2

--- app.js
+++ app.js
@@ -26,51 +26,20 @@ function policeaudio(){
 }
 
 
+var dialog = document.getElementById("dialog");
 
-function dialogfunc(){
-    var dialog=document.querySelector("#dialog")
-    dialog.style.display="block"
-    audio3.play();
-$(document).ready(function() {
-    
-    $('#open-dialog').click(function() {
-      $('#dialog').fadeIn();
-    });
-  
-    $('#close-dialog').click(function(e) {
-        e.preventDefault();
-      $('#dialog').fadeOut();
-    });
-    
-  });
-}
-  var avtorizacia=document.querySelector("#open-dialog")
-  avtorizacia.addEventListener('click', dialogfunc)
-  
-  var dialog2=document.querySelector("#dialog2")
-  dialog2.addEventListener('click', dialogfunc2)
-
-  function dialogfunc2(){
-    alert("112-ში რამ დაგარეკინა? ? ? :D :D :D :D ")
-   
-   
-
-    function animateBackgroundColor() {
-        var colors = ["red", "blue", ]; // Define an array of colors to cycle through
-        var currentIndex = 0; // Variable to keep track of the current color index
-      
-        setInterval(function() {
-          document.body.style.backgroundColor = colors[currentIndex]; 
-          
-      
-          currentIndex++; 
-      
-        
-          if (currentIndex >= colors.length) {
-            currentIndex = 0;
-          }
-        }, 1000); 
-      }
-      animateBackgroundColor();
-      
+  function openDialog() {
+    dialog.style.display = "block";
   }
+
+  function closeDialog() {
+    dialog.style.display = "none";
+  }
+
+
+
+
+
+
+   
+   
