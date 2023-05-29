071d4cc5cbf0fa11f8388c57ec3e4ced3eb9c868
iurimach <119754002+iurimach@users.noreply.github.com>
2023-05-29 20:43:11 +0400

    shenishnvebit

--- app.js
+++ app.js
@@ -9,21 +9,7 @@ function passwordaudio(){
     audio2.play();
 }
 
-var audio3=document.querySelector("#audio3")
-function autorizaciaaudio(){
-    audio3.play();
-}
-var audio4=document.querySelector("#audio4")
-function zari112audio(){
-    audio4.play();
-}
 
-var audio5=document.querySelector("#audio5")
-function policeaudio(){
-    
-    audio5.play();
-    
-}
 
 
 var dialog = document.getElementById("dialog");
